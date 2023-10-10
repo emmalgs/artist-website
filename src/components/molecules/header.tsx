8267f16b6a92f65c83289a55f08ca41e23fd1128
Emma Gerigscott <elgerig@gmail.com>
2023-10-10 12:18:40 -0700

    add class names

--- src/components/molecules/header.tsx
+++ src/components/molecules/header.tsx
@@ -1,8 +1,8 @@
 function Header() {
   return (
-    <div>
+    <div className="header">
       <div>
-        <img src="https://via.placeholder.com/80" alt="menu icon" />
+        <img src="https://via.placeholder.com/80" alt="menu icon" className="menu-icon"/>
       </div>
       <h1>EMMA GERIGSCOTT</h1>
     </div>
