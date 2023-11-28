67387196fd440115ccee574949ec0b38abe97a15
Emma Gerigscott <elgerig@gmail.com>
2023-11-27 17:10:47 -0800

    Update firebase.ts to include authentication

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,19 +1,21 @@
 import { initializeApp } from 'firebase/app';
 import { FirebaseConfig } from '../types';
 import { getDatabase } from 'firebase/database';
+import { getAuth } from 'firebase/auth';
 
 const firebaseConfig: FirebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY as string,
-  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN as string,
-  databaseURL: process.env.VITE_FIREBASE_DATABASE_URL as string,
-  projectId: process.env.VITE_FIREBASE_PROJECT_ID as string,
-  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET as string,
-  messagingSenderId: process.env.VITE_FIREBASE_SENDER_ID as string,
-  appId: process.env.VITE_FIREBASE_APP_ID as string,
-  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID as string
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY as string,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN as string,
+  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL as string,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID as string,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID as string,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID as string,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID as string
 }
 
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 const db = getDatabase(app);
 
-export { db };
+export { db, auth };
