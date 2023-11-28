a28f07db4c5fe3e24dee69612c51c2554927e23d
Emma Gerigscott <elgerig@gmail.com>
2023-11-27 18:54:45 -0800

    Add artwork data retrieval from Firebase

--- src/components/cms/Home.tsx
+++ src/components/cms/Home.tsx
@@ -1,23 +1,43 @@
 import AdminHeader from "./AdminHeader";
 import AdminDashboard from "./AdminDashboard";
 import Login from "./Login";
-import { auth } from "../../services/firebase";
+import { auth, db } from "../../services/firebase";
+import { ref, onValue } from "firebase/database";
 import { useState, useEffect } from "react";
 import firebase from "firebase/compat/app";
 
 const Home = () => {
   const [user, setUser] = useState<firebase.User | null>(null);
+  const [allArt, setAllArt] = useState([]);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
     });
+
+    const artdb = ref(db, "paintings/");
+    const unSubscribe = onValue(
+      artdb,
+      (snapshot) => {
+        const data = snapshot.val();
+        const artList = [];
+        for (let id in data) {
+          artList.push({ id, ...data[id] });
+        }
+        setAllArt(artList);
+      },
+      (error) => {
+        console.log(error.message);
+      }
+    );
+    return () => unSubscribe();
   }, []);
+  console.log(allArt);
 
   return (
     <div>
-      <AdminHeader user={auth.currentUser}/>
-      {user ? <AdminDashboard /> : <Login />}
+      <AdminHeader user={auth.currentUser} />
+      {user ? <AdminDashboard artworks={allArt} /> : <Login />}
     </div>
   );
 };
