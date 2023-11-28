c0eeaf5064ffb38651b07d11b757644ef4f3c448
Emma Gerigscott <elgerig@gmail.com>
2023-11-27 17:40:03 -0800

    Add Logout component

---
+++ src/components/cms/Logout.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import { auth } from "../../services/firebase";
+
+const Logout = () => {
+  const [logoutMessage, setLogoutMessage] = useState("");
+
+  const handleLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        localStorage.removeItem("loggedIn");
+        setLogoutMessage("Logged out successfully");
+      })
+      .catch((error: { message: string }) => {
+        setLogoutMessage(`Error: ${error.message}`);
+      });
+  };
+  return (
+    <div>
+      <button onClick={handleLogout}>Logout</button>
+      <div>{logoutMessage}</div>
+    </div>
+  );
+};
+
+export default Logout;
