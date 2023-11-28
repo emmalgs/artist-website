d7a6a07d979cc61c34be39f1dedd7d8b27cba7a2
Emma Gerigscott <elgerig@gmail.com>
2023-11-28 13:55:34 -0800

    Refactor ArtCard component to display artwork details

--- src/components/cms/molecules/ArtCard.tsx
+++ src/components/cms/molecules/ArtCard.tsx
@@ -1,15 +1,19 @@
 const ArtCard = ({ artwork }) => {
   return (
-    <div>
+    <div className="art-card">
       <div>
-        <img src={artwork.imgSrc} alt={artwork.title} />
+        <img src={artwork.imgSrc} alt={artwork.title} height="200px" />
       </div>
-      <h4>{artwork.title}</h4>
-      <p>{artwork.dimensions}</p>
-      <p>{artwork.medium}</p>
-      <p>{artwork.price}</p>
+      <h4>Title: {artwork.title}</h4>
+      <p>Dimensions: {artwork.dimensions}</p>
+      <p>Medium: {artwork.medium}</p>
+      <p>{artwork.price ? `Price: ${artwork.price}` : ""}</p>
+      <div>
+        <button>Edit</button>
+        <button>Delete</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default ArtCard;
