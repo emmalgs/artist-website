0001a5f59bea26303af8143051eb70f9385c2f72
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 10:33:03 -0800

    Refactor IconButton component to use TypeScript

--- src/components/cms/atoms/IconButton.tsx
+++ src/components/cms/atoms/IconButton.tsx
@@ -1,19 +1,26 @@
-const IconButton = ({ icon, type }) => {
-  let iconStyle = ""
+import React from 'react';
+
+interface IconButtonProps {
+  type: "delete" | "edit" | "add";
+  icon: React.ReactNode;
+}
+
+const IconButton: React.FC<IconButtonProps> = ({ type, icon }) => {
+  let iconStyle = "";
 
   if (type === "delete") {
-    iconStyle = "text-red-700 hover:text-red-600"
+    iconStyle = "text-red-700 hover:text-red-600";
   } else if (type === "edit") {
-    iconStyle = "text-green-700 hover:text-green-600"
+    iconStyle = "text-green-700 hover:text-green-600";
   } else if (type === "add") {
-    iconStyle = "text-blue-700 hover:text-blue-600"
+    iconStyle = "text-blue-700 hover:text-blue-600";
   }
 
   return (
     <button className={`${iconStyle} font-bold px-1 py-1`}>
-      { icon }
+      {icon}
     </button>
-  )
-}
+  );
+};
 
 export default IconButton;
