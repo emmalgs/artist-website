782258c08617066909c0df5153178878d27c2ca7
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 10:57:01 -0800

    Update AdminHeader component styling

--- src/components/cms/molecules/AdminHeader.tsx
+++ src/components/cms/molecules/AdminHeader.tsx
@@ -3,9 +3,9 @@ import Logout from "./Logout";
 
 const AdminHeader: React.FC<AdminHeaderProps> = ({ user }) => {
   return (
-    <div className="fixed bg-amber-100 w-full p-4">
+    <div className="fixed bg-white shadow-md w-full p-4">
       <div className="flex w-full justify-between">
-        <h1>{user ? `Welcome back, ${user.email}` : ""}</h1>
+        <h1 className="text-lg text-stone-500 w-full m-auto font-bold">{user ? `Welcome back, ${user.email}` : ""}</h1>
         {user ? <Logout /> : <p>Please login to continue</p>}
       </div>
     </div>
