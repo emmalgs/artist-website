4de671d62c5890450bff190de257bafd178226bc
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 09:54:58 -0800

    Refactor login page component

---
+++ src/components/cms/pages/Login.tsx
@@ -0,0 +1,53 @@
+import { auth } from "../../../services/firebase";
+import Form from "../organisms/Form";
+import { FormEvent, useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const Login = () => {
+  const [loginMessage, setLoginMessage] = useState("");
+
+  const handleLogin = (event: FormEvent) => {
+    event.preventDefault();
+    const target = event.target as typeof event.target & {
+      email: { value: string };
+      password: { value: string };
+    };
+    const email = target.email.value;
+    const password = target.password.value;
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setLoginMessage(`Welcome ${userCredential.user.email}`);
+      })
+      .catch((error: { message: string }) => {
+        setLoginMessage(`Error: ${error.message}`);
+      });
+  };
+
+  const inputs = [
+    {
+      name: "email",
+      type: "email",
+      text: "Email",
+      placeholder: "Email",
+    },
+    {
+      name: "password",
+      type: "password",
+      text: "Password",
+      placeholder: "Password",
+    },
+  ];
+
+  return (
+    <div>
+      <Form handleSubmit={handleLogin} buttonText="Login" sections={inputs} />
+      <p>{loginMessage}</p>
+      <p className="text-center text-gray-500 text-xs">
+        &copy; 2023 Emma Gerigscott, Ladies IT Department. All rights reserved
+      </p>
+    </div>
+  );
+};
+
+export default Login;
