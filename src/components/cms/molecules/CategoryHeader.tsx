71a5bd4fada8a9c59440a0551b02fee2bbd7b804
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 10:32:10 -0800

    Refactor CategoryHeader component

--- src/components/cms/molecules/CategoryHeader.tsx
+++ src/components/cms/molecules/CategoryHeader.tsx
@@ -7,11 +7,13 @@ interface CategoryHeaderProps {
 
 const CategoryHeader: React.FC<CategoryHeaderProps> = ({ category }) => {
   return (
-    <div className="flex w-full gap-6 justify-start">
-      <h2 className="text-3xl">{category}</h2>
-      <IconButton icon={<FaPlus />} />
+    <div className="flex w-full gap-6 justify-between border-b-2 border-stone-700">
+      <h2 className="text-3xl pl-5">{category}</h2>
+      <div className="pr-5">
+        <IconButton icon={<FaPlus />} type="add" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default CategoryHeader;
