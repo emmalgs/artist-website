ab647d7d90d3a17651baf944484c9b320f4d24e6
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 09:19:38 -0800

    Add type prop to Button component

--- src/components/cms/atoms/Button.tsx
+++ src/components/cms/atoms/Button.tsx
@@ -1,6 +1,7 @@
-const Button = ({ text, action }) => {
+const Button = ({ text, action, type }) => {
   return (
     <button
+      type={type}
       className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
       onClick={action}
     >
