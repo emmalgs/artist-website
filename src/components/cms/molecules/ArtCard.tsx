1dc9f37d242ed3ac3a96e45beaea665143d5f23b
Emma Gerigscott <elgerig@gmail.com>
2023-11-28 16:56:42 -0800

    Refactor ArtCard component to use IconButton component

--- src/components/cms/molecules/ArtCard.tsx
+++ src/components/cms/molecules/ArtCard.tsx
@@ -1,16 +1,26 @@
+import IconButton from "../atoms/IconButton";
+import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
+
 const ArtCard = ({ artwork }) => {
   return (
-    <div className="art-card">
+    <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-4/5 bg-slate-200">
       <div>
-        <img src={artwork.imgSrc} alt={artwork.title} className="object-cover h-48 w-48" />
+        <img
+          src={artwork.imgSrc}
+          alt={artwork.title}
+          className="object-cover h-48 w-48"
+        />
       </div>
-      <h4>Title: {artwork.title}</h4>
-      <p>Dimensions: {artwork.dimensions}</p>
-      <p>Medium: {artwork.medium}</p>
-      <p>{artwork.price ? `Price: ${artwork.price}` : ""}</p>
       <div>
-        <button>Edit</button>
-        <button>Delete</button>
+        <h4>Title: <span>{artwork.title}</span></h4>
+        <p>Dimensions: {artwork.dimensions}</p>
+        <p>Medium: {artwork.medium}</p>
+        <p>{artwork.price ? `Price: ${artwork.price}` : ""}</p>
+      <div className="flex h-20 gap-3 px-3 py-4">
+        <IconButton icon={<FaEdit />} />
+        <IconButton icon={<FaTrash />} />
+      </div>
       </div>
     </div>
   );
