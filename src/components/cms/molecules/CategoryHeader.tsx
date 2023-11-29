1caf5b37759e0d05f26ce4d769ac5ae8b9852f49
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 10:09:05 -0800

    add ts interface

--- src/components/cms/molecules/CategoryHeader.tsx
+++ src/components/cms/molecules/CategoryHeader.tsx
@@ -1,7 +1,11 @@
 import IconButton from "../atoms/IconButton";
 import { FaPlus } from "react-icons/fa";
 
-const CategoryHeader = ({ category }) => {
+interface CategoryHeaderProps {
+  category: string;
+}
+
+const CategoryHeader: React.FC<CategoryHeaderProps> = ({ category }) => {
   return (
     <div className="flex w-full gap-6 justify-start">
       <h2 className="text-3xl">{category}</h2>
