703591a6d35189867894435abed9620b1ce00567
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 10:32:05 -0800

    Refactor ArtCard component to use TypeScript and update IconButton props

--- src/components/cms/molecules/ArtCard.tsx
+++ src/components/cms/molecules/ArtCard.tsx
@@ -1,8 +1,10 @@
+import React from "react";
+import { Artwork } from "../../../types";
 import IconButton from "../atoms/IconButton";
 import { FaEdit } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa";
 
-const ArtCard = ({ artwork }) => {
+const ArtCard: React.FC<Artwork> = ({ artwork }) => {
   return (
     <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-4/5 bg-slate-200">
       <div>
@@ -13,14 +15,14 @@ const ArtCard = ({ artwork }) => {
         />
       </div>
       <div>
-        <h4>Title: <span>{artwork.title}</span></h4>
+        <h4>
+          Title: <span>{artwork.title}</span>
+        </h4>
         <p>Dimensions: {artwork.dimensions}</p>
         <p>Medium: {artwork.medium}</p>
         <p>{artwork.price ? `Price: ${artwork.price}` : ""}</p>
-      <div className="flex h-20 gap-3 px-3 py-4">
-        <IconButton icon={<FaEdit />} />
-        <IconButton icon={<FaTrash />} />
-      </div>
+        <IconButton icon={<FaEdit />} type="edit" />
+        <IconButton icon={<FaTrash />} type="delete" />
       </div>
     </div>
   );
