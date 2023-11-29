5045edf9561ea5b243d2a61ccffcf9651a0bb542
Emma Gerigscott <elgerig@gmail.com>
2023-11-28 17:03:44 -0800

    Add user greeting in header

--- src/components/cms/molecules/AdminHeader.tsx
+++ src/components/cms/molecules/AdminHeader.tsx
@@ -1,10 +1,13 @@
-import { AdminHeaderProps } from '../../../types';
-import Logout from './Logout';
+import { AdminHeaderProps } from "../../../types";
+import Logout from "./Logout";
 
 const AdminHeader: React.FC<AdminHeaderProps> = ({ user }) => {
   return (
-    <div>
-      {user ? <Logout /> : <p>Please login to continue</p>}
+    <div className="fixed bg-amber-100 w-full p-4">
+      <div className="flex w-full justify-between">
+        <h1>{user ? `Welcome back, ${user.email}` : ""}</h1>
+        {user ? <Logout /> : <p>Please login to continue</p>}
+      </div>
     </div>
   );
 };
