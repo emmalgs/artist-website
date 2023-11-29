c6c019f79cbf3a9ff60d23eca01eca022e2ed1b1
Emma Gerigscott <elgerig@gmail.com>
2023-11-29 10:57:07 -0800

    Update ArtCard styling

--- src/components/cms/molecules/ArtCard.tsx
+++ src/components/cms/molecules/ArtCard.tsx
@@ -6,7 +6,7 @@ import { FaTrash } from "react-icons/fa";
 
 const ArtCard: React.FC<Artwork> = ({ artwork }) => {
   return (
-    <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-4/5 bg-slate-200">
+    <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-full bg-stone-100 border-b-2">
       <div>
         <img
           src={artwork.imgSrc}
