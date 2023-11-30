77782742e9a953663edce0276400513c9280bf66
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 10:52:11 -0800

    Refactor FormLabel component to use labelId instead of type

--- src/components/cms/atoms/FormLabel.tsx
+++ src/components/cms/atoms/FormLabel.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 interface FormLabelProps {
-  type: string;
+  labelId: string;
   text: string;
 }
 
-const FormLabel: React.FC<FormLabelProps> = ({ type, text }) => {
+const FormLabel: React.FC<FormLabelProps> = ({ text, labelId }) => {
   return (
     <label
       className="block text-gray-700 text-sm font-bold mb-2"
-      htmlFor={type}
+      htmlFor={labelId}
     >
       {text}
     </label>
