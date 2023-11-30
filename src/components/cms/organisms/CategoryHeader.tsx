8017f91cc7f70783e2336454591415d536220c05
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 13:44:49 -0800

    Refactor CategoryHeader component to dynamically generate inputs

--- src/components/cms/organisms/CategoryHeader.tsx
+++ src/components/cms/organisms/CategoryHeader.tsx
@@ -1,117 +1,131 @@
-import IconButton from "../atoms/IconButton";
-import Form from "./Form";
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { useState } from "react";
-import { FaPlus } from "react-icons/fa";
-import { Painting } from "../../../types";
+import Form from "../molecules/Form";
+// import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+// import { useState } from "react";
+import { FormInputProps } from "../../../types";
+import { db } from "../../../services/firebase";
+import { ref, onValue } from "firebase/database";
+import { useState, useEffect } from "react";
 
-interface CategoryHeaderProps {
-  category: string;
-  handleSubmit: (art: Painting, header: string) => void;
-}
+const CategoryForm = ({ category }) => {
+  const [inputs, setInputs] = useState<FormInputProps[]>([]);
+  const dbInputsLocation = `${category}/inputs`;
 
-const CategoryHeader: React.FC<CategoryHeaderProps> = ({
-  category,
-  handleSubmit,
-}) => {
-  const [formVisible, setFormVisible] = useState(false);
-  const [form, setForm] = useState<React.ReactNode>(null);
-  const [artwork, setArtwork] = useState<Painting | (() => Painting)>({});
-
-  let uploadedImg: File;
-  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      uploadedImg = e.target.files[0];
-    }
-  };
-
-  const handleAddSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    const { title, dimensions, medium, price } = e.target as typeof e.target & {
-      title: { value: string };
-      dimensions: { value: string };
-      medium: { value: string };
-      price: { value: string };
-    };
-    console.log(title.value, dimensions.value, medium.value, price.value);
-    setFormVisible(!formVisible);
-    const storage = getStorage();
-    const storageRef = ref(storage, `${uploadedImg.name}`);
-    uploadBytes(storageRef, uploadedImg).then((snapshot) => {
-      console.log("Uploaded a blob or file!");
-      getDownloadURL(snapshot.ref).then((url) => {
-        setArtwork({
-          title: title.value,
-          dimensions: dimensions.value,
-          medium: medium.value,
-          price: price.value,
-          imgSrc: url,
+  useEffect(() => {
+    const inputData = ref(db, dbInputsLocation);
+    const unSubscribe = onValue(inputData, (snapshot) => {
+      const d = snapshot.val();
+      const keys = Object.keys(d);
+      const inputList: FormInputProps[] = [];
+      keys.forEach(
+        (key, index) => {
+          const input: FormInputProps = {
+            ...d[key],
+            id: keys[index],
+          };
+          inputList.push(input);
         });
-      });
-    });
-    handleSubmit(artwork as Painting, category);
+        setInputs(inputList);
+      },
+        (error) => {
+          console.log(error.message);
+        }
+      );
+    return () => unSubscribe();
+  });
+
+  // let uploadedImg: File;
+  // const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  //   if (e.target.files && e.target.files[0]) {
+  //     uploadedImg = e.target.files[0];
+  //   }
+  // };
+
+  // const handleAddSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   const { title, dimensions, medium, price } = e.target as typeof e.target & {
+  //     title: { value: string };
+  //     dimensions: { value: string };
+  //     medium: { value: string };
+  //     price: { value: string };
+  //   };
+  //   console.log(title.value, dimensions.value, medium.value, price.value);
+  //   setFormVisible(!formVisible);
+  //   const storage = getStorage();
+  //   const storageRef = ref(storage, `${uploadedImg.name}`);
+  //   uploadBytes(storageRef, uploadedImg).then((snapshot) => {
+  //     console.log("Uploaded a blob or file!");
+  //     getDownloadURL(snapshot.ref).then((url) => {
+  //       setArtwork({
+  //         title: title.value,
+  //         dimensions: dimensions.value,
+  //         medium: medium.value,
+  //         price: price.value,
+  //         imgSrc: url,
+  //       });
+  //     });
+  //   });
+  //   handleSubmit(artwork as Painting, category);
+  // };
+  const handleSubmit = () => {
+    console.log("submit");
   };
 
-  const inputs = [
-    {
-      type: "text",
-      text: "Title",
-      name: "title",
-      placeholder: "Title",
-    },
-    {
-      type: "text",
-      text: "Dimensions",
-      name: "dimensions",
-      placeholder: "Dimensions",
-    },
-    {
-      type: "text",
-      text: "Medium",
-      name: "medium",
-      placeholder: "Medium",
-    },
-    {
-      type: "text",
-      text: "Price",
-      name: "price",
-      placeholder: "Price",
-    },
-    {
-      type: "file",
-      text: "Image",
-      name: "image",
-      placeholder: "",
-      accept: "image/*",
-      handleChange: handleImageChange,
-    },
-  ];
+  // const inputs = [
+  //   {
+  //     type: "text",
+  //     text: "Title",
+  //     name: "title",
+  //     placeholder: "Title",
+  //   },
+  //   {
+  //     type: "text",
+  //     text: "Dimensions",
+  //     name: "dimensions",
+  //     placeholder: "Dimensions",
+  //   },
+  //   {
+  //     type: "text",
+  //     text: "Medium",
+  //     name: "medium",
+  //     placeholder: "Medium",
+  //   },
+  //   {
+  //     type: "text",
+  //     text: "Price",
+  //     name: "price",
+  //     placeholder: "Price",
+  //   },
+  //   {
+  //     type: "file",
+  //     text: "Image",
+  //     name: "image",
+  //     placeholder: "",
+  //     accept: "image/*",
+  //     handleChange: handleImageChange,
+  //   },
+  // ];
 
-  const handleAddClick = () => {
-    setFormVisible(!formVisible);
-    formVisible ? setForm(null) : 
-    setForm(
-      <Form
-        buttonText="Add new work"
-        sections={inputs}
-        handleSubmit={handleAddSubmit}
-      />
-    );
-  };
+  // const handleAddClick = () => {
+  //   setFormVisible(!formVisible);
+  //   formVisible ? setForm(null) :
+  //   setForm(
+  //     <Form
+  //       buttonText="Add new work"
+  //       sections={inputs}
+  //       handleSubmit={handleAddSubmit}
+  //     />
+  //   );
+  // };
 
   return (
     <div>
-      <div className="flex w-full gap-6 justify-between border-b-2 border-stone-700">
-        <h2 className="text-3xl pl-5">{category}</h2>
-        <div className="pr-5">
-          <IconButton icon={<FaPlus />} type="add" action={handleAddClick} />
-        </div>
-      </div>
-      <div className="fixed right-10 top-20">
-      {form ? form : null}
-      </div>
+      <Form
+        sections={inputs}
+        handleSubmit={handleSubmit}
+        buttonText="Add new work"
+      />
     </div>
   );
 };
 
-export default CategoryHeader;
+export default CategoryForm;
