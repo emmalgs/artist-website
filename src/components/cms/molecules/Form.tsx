6477b26645855b2f9129f2b9b83afce39aa7a39a
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 13:44:20 -0800

    Add Form component

---
+++ src/components/cms/molecules/Form.tsx
@@ -0,0 +1,29 @@
+import FormSection from "./FormSection";
+import Button from "../atoms/Button";
+import { FormProps } from "../../../types";
+
+const Form: React.FC<FormProps> = ({ sections, buttonText, handleSubmit }) => {
+  return (
+    <div className="w-full max-w-xs m-auto pt-2">
+      <form onSubmit={(e) => handleSubmit(e)} className="bg-white shadow-md rounded px-4 pt-4 pb-4 mb-4">
+        {sections.map((section, index) => {
+          return (
+            <FormSection
+              key={index}
+              type={section.type}
+              text={section.text}
+              name={section.name}
+              placeholder={section.placeholder}
+              handleInputEvent={section.handleChange}
+            />
+          );
+        })}
+        <div className="flex items-center justify-between">
+        <Button text={buttonText} type="submit" />
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
