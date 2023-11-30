a9f407755981fbecfc60142ead7bc54c0f319693
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 12:50:52 -0800

    Update form styling

--- src/components/cms/organisms/Form.tsx
+++ src/components/cms/organisms/Form.tsx
@@ -4,8 +4,8 @@ import { FormProps } from "../../../types";
 
 const Form: React.FC<FormProps> = ({ sections, buttonText, handleSubmit }) => {
   return (
-    <div className="w-full max-w-xs m-auto pt-8">
-      <form onSubmit={(e) => handleSubmit(e)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+    <div className="w-full max-w-xs m-auto pt-2">
+      <form onSubmit={(e) => handleSubmit(e)} className="bg-white shadow-md rounded px-4 pt-4 pb-4 mb-4">
         {sections.map((section, index) => {
           return (
             <FormSection
