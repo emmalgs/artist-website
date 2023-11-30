011357f6e34da55366ac7d863054990bae2f156f
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 10:52:06 -0800

    Add accept and handleChange props to FormInput component

--- src/components/cms/atoms/FormInput.tsx
+++ src/components/cms/atoms/FormInput.tsx
@@ -1,12 +1,14 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 
 interface FormInputProps {
   type: string;
   name: string;
   placeholder: string;
+  accept?: string;
+  handleChange?: (e: ChangeEvent<HTMLInputElement>) => void;
 }
 
-const FormInput: React.FC<FormInputProps> = ({ type, name, placeholder }) => {
+const FormInput: React.FC<FormInputProps> = ({ type, name, placeholder, accept = "", handleChange }) => {
   return (
     <input
       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -14,7 +16,8 @@ const FormInput: React.FC<FormInputProps> = ({ type, name, placeholder }) => {
       name={name}
       id={name}
       placeholder={placeholder}
-    />
+      accept={accept}
+      onChange={handleChange} />
   );
 };
 
