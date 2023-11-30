9e178581c548bc19dfe8d3c196493e8385b2182f
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 10:52:17 -0800

    Refactor AllArtCard component and add handleAddSubmit function

--- src/components/cms/molecules/AllArtCard.tsx
+++ src/components/cms/molecules/AllArtCard.tsx
@@ -1,14 +1,28 @@
 import ArtCard from "./ArtCard";
-import CategoryHeader from "./CategoryHeader";
 import { Artworks } from "../../../types";
+import { db } from "../../../services/firebase";
+import CategoryHeader from "../organisms/CategoryHeader";
+import { Painting } from "../../../types";
+import { ref, push, set } from "firebase/database";
 
 const AllArtCard: React.FC<Artworks> = ({ artworks }) => {
+  const handleAddSubmit = (art: Painting, header: string) => {
+    const newDataRef = push(ref(db, `${header}`));
+    set(newDataRef, art)
+    .then(() => {
+      console.log("Data saved successfully!");
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
+
   return (
     <div className="flex flex-col bg-stone-100 w-2/3 p-6 m-6 shadow-md rounded-md">
         {Object.keys(artworks).map((category) => {
           return (
             <div key={category}>
-              <CategoryHeader category={category} />
+              <CategoryHeader category={category} handleSubmit={handleAddSubmit} />
               {Object.keys(artworks[category]).map((artwork) => {
                 return (
                   <div key={artwork}>
