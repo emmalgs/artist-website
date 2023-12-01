a5c8bd4329064db87f570f7e6a5d3bb96519a766
Emma Gerigscott <elgerig@gmail.com>
2023-12-01 10:29:49 -0800

    Refactor Button component

--- src/components/cms/atoms/Button.tsx
+++ src/components/cms/atoms/Button.tsx
@@ -1,21 +1,20 @@
 import React from 'react';
+  interface ButtonProps {
+    type: "button" | "submit" | "reset" | undefined;
+    text: string;
+    action?: () => void;
+  }
 
-interface ButtonProps {
-  type: "button" | "submit" | "reset" | undefined;
-  text: string;
-  action?: () => void;
-}
-
-const Button: React.FC<ButtonProps> = ({ text, action, type }) => {
-  return (
-    <button
-      type={type}
-      className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
-      onClick={action}
-    >
-      {text}
-    </button>
-  );
-};
+  const Button: React.FC<ButtonProps> = ({ text, action, type }) => {
+    return (
+      <button
+        type={type}
+        className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+        onClick={action}
+      >
+        {text}
+      </button>
+    );
+  };
 
 export default Button;
