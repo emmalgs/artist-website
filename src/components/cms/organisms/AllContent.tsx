dc8c97dd660508a6ad5ac12dac8c85a36ba5d286
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 17:19:10 -0800

    Refactor AllContent component to use ContentCard instead of ArtCard

--- src/components/cms/organisms/AllContent.tsx
+++ src/components/cms/organisms/AllContent.tsx
@@ -1,23 +1,38 @@
-import ArtCard from "../molecules/ArtCard";
+import ContentCard from "../molecules/ContentCard";
 import ContentHeader from "../molecules/ContentHeader";
 
 const AllContent = ({ content }) => {
+  const showItems = (category) => {
+    if (content[category].collection) {
+      return Object.keys(content[category].collection).map((artwork) => {
+        return (
+          <div key={artwork}>
+            <ContentCard
+              category={category}
+              artwork={content[category].collection[artwork]}
+            />
+          </div>
+        );
+      });
+    } else {
+      return (
+        <div className="flex justify-center">
+          <p className="text-2xl m-4">Nothing in {category.toLowerCase()}. Add items to {category}</p>
+        </div>
+      );
+    }
+  };
+
   return (
     <div className="flex flex-col bg-stone-100 w-2/3 p-6 m-6 shadow-md rounded-md">
-        {Object.keys(content).map((category) => {
-          return (
-            <div key={category}>
-              <ContentHeader contentTitle={category} action="add" />
-              {Object.keys(content[category].collection).map((artwork) => {
-                return (
-                  <div key={artwork}>
-                    <ArtCard artwork={content[category].collection[artwork]} />
-                  </div>
-                );
-              })}
-            </div>
-          );
-        })}
+      {Object.keys(content).map((category) => {
+        return (
+          <div key={category}>
+            <ContentHeader contentTitle={category} action="add" />
+            {showItems(category)}
+          </div>
+        );
+      })}
     </div>
   );
 };
