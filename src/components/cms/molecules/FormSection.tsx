34dd362c511b78cbae9e37d30819bb240d1892e9
Emma Gerigscott <elgerig@gmail.com>
2023-12-01 14:21:03 -0800

    Refactor FormSection component

--- src/components/cms/molecules/FormSection.tsx
+++ src/components/cms/molecules/FormSection.tsx
@@ -1,23 +1,27 @@
 import React from "react";
 import FormInput from "../atoms/FormInput";
 import FormLabel from "../atoms/FormLabel";
+import { FormSectionProps } from "../../../types";
 
-interface FormSectionProps {
-  type: string;
-  text: string;
-  name: string;
-  placeholder: string;
-  handleInputEvent?: (e: React.ChangeEvent<HTMLInputElement>) => void;
-}
-
-const FormSection: React.FC<FormSectionProps> = ({ type, text, name, placeholder, handleInputEvent }) => {
+const FormSection: React.FC<FormSectionProps> = ({
+  type,
+  text,
+  name,
+  placeholder,
+  handleInputEvent,
+}) => {
   return (
     <div className="pt-4 pb-2">
       <div className="mb-4">
         <FormLabel text={text} labelId={name} />
       </div>
       <div className="md:w-5/6">
-        <FormInput type={type} name={name} placeholder={placeholder} handleChange={handleInputEvent}/>
+        <FormInput
+          type={type}
+          name={name}
+          placeholder={placeholder}
+          handleChange={handleInputEvent}
+        />
       </div>
     </div>
   );
