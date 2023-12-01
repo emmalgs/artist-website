4c4e5ff579343ad874f7edd5e11ed1a47df3a0da
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 17:18:59 -0800

    rename ContentCard component

--- src/components/cms/molecules/ContentCard.tsx
+++ src/components/cms/molecules/ContentCard.tsx
@@ -2,30 +2,33 @@ import IconButton from "../atoms/IconButton";
 import CardInfo from "../atoms/CardInfo";
 import { FaEdit } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa";
+import { deleteItemFromCollection } from "../../../services/database";
 
-const ArtCard = ({ artwork }) => {
+const ContentCard = ({ artwork, category }) => {
   return (
     <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-full bg-stone-100 border-b-2">
       <div>
         <img
-          src={artwork.imgSrc}
+          src={artwork.image}
           alt="image of art piece"
           className="object-cover h-48 w-48"
         />
       </div>
       <div>
         {Object.keys(artwork).map((key) => {
-          return (
-            <div key={key}>
-              <CardInfo text={artwork[key]} />
-            </div>
-          );
+          if (key !== "image" && key !== "id") {
+            return (
+              <div key={key}>
+                <CardInfo text={artwork[key]} />
+              </div>
+            );
+          }
         })}
         <IconButton icon={<FaEdit />} type="edit" />
-        <IconButton icon={<FaTrash />} type="delete" />
+        <IconButton icon={<FaTrash />} type="delete" action={() => deleteItemFromCollection(category, artwork.id)}/>
       </div>
     </div>
   );
 };
 
-export default ArtCard;
+export default ContentCard;
