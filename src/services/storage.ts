237c43d66261c3e9c649c0725dd88f8012bec61c
Emma Gerigscott <elgerig@gmail.com>
2023-12-01 14:21:33 -0800

    Add getAllImagesInStorage function to retrieve all images from storage

--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,4 +1,4 @@
-import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { getStorage, ref, uploadBytes, getDownloadURL, listAll } from "firebase/storage";
 
 export const getImageURL = async (file: File) => {
   const storage = getStorage();
@@ -6,4 +6,31 @@ export const getImageURL = async (file: File) => {
   await uploadBytes(storageRef, file);
   const url = await getDownloadURL(storageRef);
   return url;
+};
+
+export const getAllImagesInStorage = async () => {
+  try {
+    const imagePaths = await getImageRefs();
+    const downloadURLPromises = imagePaths.map(async (imageRef) => {
+      try {
+        const url = await getDownloadURL(imageRef);
+        return url;
+      } catch (error) {
+        console.log(error.message);
+        return null;
+      }
+    });
+    const downloadURLs = await Promise.all(downloadURLPromises);
+    return downloadURLs;
+  } catch (err) {
+    console.log(err.message);
+    return [];
+  }
+};
+
+const getImageRefs = async () => {
+  const storage = getStorage();
+  const listRef = ref(storage);
+  const res = await listAll(listRef);
+  return res.items;
 }
