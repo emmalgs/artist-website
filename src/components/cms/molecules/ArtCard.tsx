cce331acefd755cddf277d9da6d7693283eb7e58
Emma Gerigscott <elgerig@gmail.com>
2023-11-30 16:15:28 -0800

    Refactor ArtCard component to use CardInfo atom

--- src/components/cms/molecules/ArtCard.tsx
+++ src/components/cms/molecules/ArtCard.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { Artwork } from "../../../types";
 import IconButton from "../atoms/IconButton";
+import CardInfo from "../atoms/CardInfo";
 import { FaEdit } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa";
 
-const ArtCard: React.FC<Artwork> = ({ artwork }) => {
+const ArtCard = ({ artwork }) => {
   return (
     <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-full bg-stone-100 border-b-2">
       <div>
         <img
           src={artwork.imgSrc}
-          alt={artwork.title}
+          alt="image of art piece"
           className="object-cover h-48 w-48"
         />
       </div>
       <div>
-        <h4>
-          Title: <span>{artwork.title}</span>
-        </h4>
-        <p>Dimensions: {artwork.dimensions}</p>
-        <p>Medium: {artwork.medium}</p>
-        <p>{artwork.price ? `Price: ${artwork.price}` : ""}</p>
+        {Object.keys(artwork).map((key) => {
+          return (
+            <div key={key}>
+              <CardInfo text={artwork[key]} />
+            </div>
+          );
+        })}
         <IconButton icon={<FaEdit />} type="edit" />
         <IconButton icon={<FaTrash />} type="delete" />
       </div>
