4d6597279e22991c54591611a58399a6176f521d
Emma Gerigscott <elgerig@gmail.com>
2023-12-04 15:08:47 -0800

    Add disabled prop to Button component

--- src/components/cms/atoms/Button.tsx
+++ src/components/cms/atoms/Button.tsx
@@ -3,14 +3,17 @@ import React from 'react';
     type: "button" | "submit" | "reset" | undefined;
     text: string;
     action?: () => void;
+    disabled? : boolean;
   }
 
-  const Button: React.FC<ButtonProps> = ({ text, action, type }) => {
+  const Button: React.FC<ButtonProps> = ({ text, action, type, disabled }) => {
     return (
       <button
+      style={disabled ? {opacity: "50", cursor: "not-allowed"} : {opacity: "100"}}
         type={type}
         className="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
         onClick={action}
+        disabled={disabled}
       >
         {text}
       </button>
