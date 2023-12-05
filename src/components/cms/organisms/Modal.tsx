b3a56cb941beb6e357ff2b431d2d7822bc9adcc6
Emma Gerigscott <elgerig@gmail.com>
2023-12-05 14:15:28 -0800

    Refactor Modal component styles

--- src/components/cms/organisms/Modal.tsx
+++ src/components/cms/organisms/Modal.tsx
@@ -1,21 +1,12 @@
 import ModalHeader from "../molecules/ModalHeader";
-import IconButton from "../atoms/IconButton";
-import { FaPlus } from "react-icons/fa";
 
 const Modal = ({ title, onClose, modalBody }) => {
   return (
-    <div className="fixed flex flex-col inset-6 ml-8 overflow-scroll max-w-screen-md bg-stone-100">
+    <div className="fixed flex flex-col inset-6 ml-8 overflow-scroll max-w-screen-md bg-stone-100 bg-opacity-50">
       <div className="p-2 w-full flex justify-between">
         <ModalHeader title={title} onClose={onClose} />
       </div>
       <div className="flex flex-wrap justify-center">{modalBody}</div>
-      <div className="fixed m-12 border-2 rounded-full border-blue-400 hover:border-green-500">
-        <IconButton
-          icon={<FaPlus />}
-          type="add"
-          action={onClose}
-        />
-      </div>
     </div>
   );
 };
