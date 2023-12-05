a9fd4daa4ac997d4ad3bee5e3586e0514af4def2
Emma Gerigscott <elgerig@gmail.com>
2023-12-05 14:15:32 -0800

    Update Form component styling

--- src/components/cms/molecules/Form.tsx
+++ src/components/cms/molecules/Form.tsx
@@ -4,7 +4,7 @@ import { FormProps } from "../../../types";
 
 const Form: React.FC<FormProps> = ({ sections, buttonText, handleSubmit }) => {
   return (
-    <div className="w-full max-w-xs m-auto pt-2">
+    <div className="w-full max-w-xs m-auto pt-2 overflow-scroll">
       <form onSubmit={(e) => handleSubmit(e)} className="bg-white shadow-md rounded px-4 pt-4 pb-4 mb-4">
         {sections.map((section, index) => {
           return (
