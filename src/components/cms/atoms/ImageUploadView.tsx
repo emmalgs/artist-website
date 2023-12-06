978012fe6e1419b639289b7faf0f76494033452d
Emma Gerigscott <elgerig@gmail.com>
2023-12-05 16:34:12 -0800

    Update ImageUploadView styling

--- src/components/cms/atoms/ImageUploadView.tsx
+++ src/components/cms/atoms/ImageUploadView.tsx
@@ -4,10 +4,10 @@ import { FaTimesCircle } from "react-icons/fa";
 const ImageUploadView = ({ imgSrc, deleteUpload }) => {
   return (
     <div className="flex flex-col w-48 m-3">
-      <div className="absolute pl-44 -mt-4 z-10">
+      <div className="pl-44 z-10">
         <IconButton icon={<FaTimesCircle />} type="close" action={deleteUpload} />
       </div>
-      <img src={imgSrc} alt="uploaded image" className="object-cover h-48 w-48 border-2 p-2 rounded-sm border-stone-200" />
+      <img src={imgSrc} alt="uploaded image" className="object-cover h-48 w-48 border-2 p-2 rounded-sm border-stone-200 -mt-5" />
     </div>
   );
 };
