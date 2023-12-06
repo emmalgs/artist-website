e06183309e2424b40a7cb8b73ef15c02f36d1ca5
Emma Gerigscott <elgerig@gmail.com>
2023-12-05 16:13:50 -0800

    Update Modal width to 5/6

--- src/components/cms/organisms/Modal.tsx
+++ src/components/cms/organisms/Modal.tsx
@@ -2,7 +2,7 @@ import ModalHeader from "../molecules/ModalHeader";
 
 const Modal = ({ title, onClose, modalBody }) => {
   return (
-    <div className="fixed flex flex-col inset-6 ml-8 overflow-scroll max-w-screen-md bg-stone-100 bg-opacity-50">
+    <div className="fixed flex flex-col inset-6 ml-8 overflow-scroll w-5/6 bg-stone-100 bg-opacity-50">
       <div className="p-2 w-full flex justify-between">
         <ModalHeader title={title} onClose={onClose} />
       </div>
