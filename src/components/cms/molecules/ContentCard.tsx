b735901cc385c0fdc3dc714df7681bfde51596f3
Emma Gerigscott <elgerig@gmail.com>
2023-12-08 11:05:56 -0800

    Refactor ContentCard component to include edit mode

--- src/components/cms/molecules/ContentCard.tsx
+++ src/components/cms/molecules/ContentCard.tsx
@@ -3,8 +3,11 @@ import CardInfo from "../atoms/CardInfo";
 import { FaEdit } from "react-icons/fa";
 import { FaTrash } from "react-icons/fa";
 import { deleteItemFromCollection } from "../../../services/database";
+import { useState } from "react";
+import EditItemForm from "../organisms/EditItemForm";
 
 const ContentCard = ({ artwork, category }) => {
+  const [editMode, setEditMode] = useState(false);
   return (
     <div className="art-card flex justify-evenly px-2 py-4 m-4 rounded-sm w-full bg-stone-100 border-b-2">
       <div>
@@ -15,17 +18,29 @@ const ContentCard = ({ artwork, category }) => {
         />
       </div>
       <div>
-        {Object.keys(artwork).map((key) => {
-          if (key !== "image" && key !== "id") {
-            return (
-              <div key={key}>
-                <CardInfo text={artwork[key]} />
-              </div>
-            );
-          }
-        })}
-        <IconButton icon={<FaEdit />} type="edit" />
-        <IconButton icon={<FaTrash />} type="delete" action={() => deleteItemFromCollection(category, artwork.id)}/>
+        {editMode ? (
+          Object.keys(artwork).map((key) => {
+            if (key !== "image" && key !== "id") {
+              return (
+                <div key={key}>
+                  <CardInfo text={artwork[key]} />
+                </div>
+              );
+            }
+          })
+        ) : (
+          <EditItemForm category={category} item={artwork} />
+        )}
+        <IconButton
+          icon={<FaEdit />}
+          type="edit"
+          action={() => setEditMode(!editMode)}
+        />
+        <IconButton
+          icon={<FaTrash />}
+          type="delete"
+          action={() => deleteItemFromCollection(category, artwork.id)}
+        />
       </div>
     </div>
   );
