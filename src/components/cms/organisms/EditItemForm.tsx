76b77e6bc81f3f43532cfbe457c3338cb80f8a11
Emma Gerigscott <elgerig@gmail.com>
2023-12-07 17:12:35 -0800

    Add EditItemForm component

---
+++ src/components/cms/organisms/EditItemForm.tsx
@@ -0,0 +1,32 @@
+import { updateItemInCollection } from "../../../services/database";
+import Form from "../molecules/Form";
+
+const EditItemForm = ({ category, item }) => {
+  const inputs = getCategoryInputs(category);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newItem = {};
+    inputs.map((input) => {
+      if (input.name === "image") {
+        newItem[input.name] = e.target[input.name].dataset.image;
+      } else {
+        newItem[input.name] = e.target[input.name].value;
+      }
+    });
+    updateItemInCollection(category, newItem);
+  };
+
+  return (
+    <div>
+      <Form
+        sections={inputs}
+        handleSubmit={handleSubmit}
+        buttonText="Add"
+        values={item}
+      />
+    </div>
+  );
+};
+
+export default EditItemForm;
