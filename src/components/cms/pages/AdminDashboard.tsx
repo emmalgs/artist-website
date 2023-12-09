a48df42656cc5e72167c1de4402d214af5d67084
Emma Gerigscott <elgerig@gmail.com>
2023-12-08 16:19:58 -0800

    Refactor AdminDashboard component to use TypeScript props

--- src/components/cms/pages/AdminDashboard.tsx
+++ src/components/cms/pages/AdminDashboard.tsx
@@ -1,8 +1,8 @@
+import { AdminDashboardProps } from "../../../types";
 import ContentHeader from "../organisms/ContentHeader";
 import AllContent from "./AllContent";
 
-
-const AdminDashboard = ({ content }) => {
+const AdminDashboard: React.FC<AdminDashboardProps> = ({ content }) => {
   return (
     <div className="w-4/5 m-auto">
       <ContentHeader action="add" contentTitle="All Website Content"/>
