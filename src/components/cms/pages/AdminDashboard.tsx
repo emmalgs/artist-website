acf81282e094d5de56904566f3a8654b5591f36e
Emma Gerigscott <elgerig@gmail.com>
2023-12-08 16:03:42 -0800

    Add ContentHeader component to AdminDashboard

--- src/components/cms/pages/AdminDashboard.tsx
+++ src/components/cms/pages/AdminDashboard.tsx
@@ -1,9 +1,11 @@
+import ContentHeader from "../organisms/ContentHeader";
 import AllContent from "./AllContent";
 
 
 const AdminDashboard = ({ content }) => {
   return (
     <div className="w-4/5 m-auto">
+      <ContentHeader action="add" contentTitle="All Website Content"/>
       <AllContent content={content} />
     </div>
   );
