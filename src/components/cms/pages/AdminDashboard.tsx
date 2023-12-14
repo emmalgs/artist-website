e9dc4f40c09670805008962995ce82066aa5aa93
Emma Gerigscott <elgerig@gmail.com>
2023-12-13 17:23:23 -0800

    Remove ContentHeader component from AdminDashboard

--- src/components/cms/pages/AdminDashboard.tsx
+++ src/components/cms/pages/AdminDashboard.tsx
@@ -1,11 +1,9 @@
 import { AdminDashboardProps } from "../../../types";
-import ContentHeader from "../organisms/ContentHeader";
 import AllContent from "./AllContent";
 
 const AdminDashboard: React.FC<AdminDashboardProps> = ({ content }) => {
   return (
     <div className="w-4/5 m-auto">
-      <ContentHeader action="add" contentTitle="All Website Content"/>
       <AllContent content={content} />
     </div>
   );
