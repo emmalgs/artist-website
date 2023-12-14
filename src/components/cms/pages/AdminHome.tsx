39b6a09773b5cd628411ca19f7972e7a976c99f2
Emma Gerigscott <elgerig@gmail.com>
2023-12-13 17:23:26 -0800

    Remove unused import

--- src/components/cms/pages/AdminHome.tsx
+++ src/components/cms/pages/AdminHome.tsx
@@ -5,7 +5,6 @@ import firebase from "firebase/compat/app";
 import Login from "./Login";
 import { ref, onValue } from "firebase/database";
 import { useState, useEffect } from "react";
-import ImagesContainer from "./ImageModal";
 
 const Home = () => {
   const [user, setUser] = useState<firebase.User | null>(null);
