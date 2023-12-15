58c6d8f6245141a68241e9f988bcccecfc6dea9f
Emma Gerigscott <elgerig@gmail.com>
2023-12-14 16:41:11 -0800

    Refactor CardInfo component to display truncated text

--- src/components/cms/atoms/CardInfo.tsx
+++ src/components/cms/atoms/CardInfo.tsx
@@ -1,7 +1,20 @@
 import { CardInfoProps } from "../../../types";
 
-const CardInfo: React.FC<CardInfoProps> = ({ text }) => {
-  return <div className="text-2xl">{text}</div>;
+const CardInfo: React.FC<CardInfoProps> = ({ text, name }) => {
+  const content = () => {  
+    if (text.length > 150) {
+      return text.slice(0, 150) + "...";
+    }
+    return text;
+  }
+
+  
+  return (
+    <div className="flex gap-2">
+      <h5 className="font-bold text-xl">{name.charAt(0).toUpperCase() + name.slice(1)}:</h5>
+      <p>{content()}</p>
+    </div>
+  );
 };
 
 export default CardInfo;
